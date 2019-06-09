153dac68f5614d0f28ecd0c3f538dccfdda4e037
msppyu1 <873634131@qq.com>
2019-06-09 21:01:25 +0800

    first commit

---
+++ src/components/boddy.vue
@@ -0,0 +1,279 @@
+<template>
+<div>
+    <div class="banner">
+         <div class="container">
+           <div class="row">
+               <div  class=" col-lg-5 col-lg-offset-1  col-md-5 col-md-offset-1 ">
+                   <div class="banner-title">公司介绍</div>
+                   <div class="banner-jieshao" >
+                       Bootstrap是一款优秀的前端框架，这套框架集成了一整套的html和css的布局效
+                       果以及javascript特效效果，它简化了前端页面的开发流程，对于不太懂前端的人
+                       也可以使用这套框架来开发前端页面。bootstrap还提供了响应式布局的方案，可以
+                       使用这套方案开发一个页面能同时适配PC端和移动端，加快项目开发的进程，它是
+                       这套框架的核心亮点。Bootstrap也是一套界面规范。本次课程将通过一个典型的商
+                       业网站实例来讲述如何使用bootstrap制作响应式页面布局。
+                   </div>
+               </div>
+                <div class=" col-lg-5 col-md-5 hidden-sm  hidden-xs">
+                    <img src="../assets/person.png" class="img-responsive" alt="">
+                </div>
+           </div>
+
+     </div>
+
+
+
+
+    </div>
+    <div class="container-fluid gray1">
+        <div class="container" >
+            <h2 class="common-title text-center">解决方案</h2>
+            <div class="row">
+        <div class="col-lg-8 col-lg-offset-2 text-center">加快项目开发的进程，它是
+                       这套框架的核心亮点。Bootstrap也是一套界面规范。本次课程将通过一个典型的商
+                       业网站实例来讲述如何使用bootstrap制作响应式页面布局。本次课程将通过一个典型的商
+                       业网站实例来讲述如何使用bootstrap制作响应式页面布局</div>
+        </div>
+        <div class="row list-con ">
+            <div class="col-lg-4 col-md-6 col-sm-6 col-xs-12  list-item list01 ">
+                <h3>网络集成</h3>
+                <p>业网站实例来讲述如何使用bootstrap制作响应式页面布局</p>
+            </div>
+            <div class="col-lg-4 col-md-6 col-sm-6 col-xs-12 list-item list02">
+                <h3>网络集成</h3>
+                <p>业网站实例来讲述如何使用bootstrap制作响应式页面布局</p>
+            </div>
+            <div class="col-lg-4 col-md-6 col-sm-6 col-xs-12 list-item list03">
+                <h3>网络集成</h3>
+                <p>业网站实例来讲述如何使用bootstrap制作响应式页面布局</p>
+            </div>
+            <div class="col-lg-4 col-md-6 col-sm-6 col-xs-12 list-item list04">
+                <h3>网络集成</h3>
+                <p>业网站实例来讲述如何使用bootstrap制作响应式页面布局</p>
+            </div>
+            <div class="col-lg-4 col-md-6 col-sm-6 col-xs-12 list-item list05">
+                <h3>网络集成</h3>
+                <p>业网站实例来讲述如何使用bootstrap制作响应式页面布局</p>
+            </div>
+            <div class="col-lg-4 col-md-6 col-sm-6 col-xs-12 list-item list06">
+                <h3>网络集成</h3>
+                <p>业网站实例来讲述如何使用bootstrap制作响应式页面布局</p>
+            </div>
+            
+        </div>
+        </div>
+    </div>
+      <div class="container-fluid">
+        <div class="container" >
+            <h2 class="common-title text-center">解决方案</h2>
+            <div class="row">
+        <div class="col-lg-8 col-lg-offset-2 text-center">加快项目开发的进程，它是
+                       这套框架的核心亮点。Bootstrap也是一套界面规范。本次课程将通过一个典型的商
+                       业网站实例来讲述如何使用bootstrap制作响应式页面布局。本次课程将通过一个典型的商
+                       业网站实例来讲述如何使用bootstrap制作响应式页面布局</div>
+        </div>
+         <div class="row tupian">
+            <div class="tupian1 col-lg-3 col-sm-6 col-xs-6"></div>
+            <div class="tupian2 col-lg-3 col-sm-6 col-xs-6"></div>
+            <div class="tupian3 col-lg-3 col-sm-6 col-xs-6"></div>
+            <div class="tupian4 col-lg-3 col-sm-6 col-xs-6"></div>
+            <div class="tupian5 col-lg-3 col-sm-6 col-xs-6"></div>
+            <div class="tupian6 col-lg-3 col-sm-6 col-xs-6"></div>
+            <div class="tupian7 col-lg-3 col-sm-6 col-xs-6"></div>
+            <div class="tupian8 col-lg-3 col-sm-6 col-xs-6"></div>
+            
+         </div>
+        </div>
+        </div>
+        <div class="container-fluid hezuo">
+            <h2 class="text-center">合作伙伴</h2>
+            <p class="text-center">我们有众多的合作伙伴</p>
+            <div class="row">
+                <div class="col-lg-2 col-lg-offset-1 col-md-6   col-sm-6 logos1" ></div>
+                <div class="col-lg-2 col-md-6 col-sm-6 logos2" ></div>
+                <div class="col-lg-2 col-md-6 col-sm-6 logos3"></div>
+                <div class="col-lg-2 col-md-6 col-sm-6 logos4"></div> 
+                <div class="col-lg-2 col-md-6 col-sm-6 logos5" ></div>
+               
+            </div>
+        </div>
+        <div class="container-fluid hezuo1">
+          
+            <div class="row">
+                <div class="col-lg-3  col-md-6 col-xs-6 text-center weibu1">
+                    <h3>公司</h3>
+                    <p>关于我们</p>
+                    <p>公司招聘</p>
+                    <p>公司团队</p>
+                    <p>版权</p>
+                    <p>使用条款</p>
+                    <p>隐私政策</p>
+                    <p>联系我们</p>
+                </div>
+                <div class="col-lg-3 col-md-6 col-xs-6 text-center weibu2">
+                    <h3>公司</h3>
+                    <p>关于我们</p>
+                    <p>公司招聘</p>
+                    <p>公司团队</p>
+                    <p>版权</p>
+                    <p>使用条款</p>
+                    <p>隐私政策</p>
+                    <p>联系我们</p>
+                </div>
+                <div class="col-lg-3 col-md-6 col-xs-6 text-center weibu3">
+                      <h3>公司</h3>
+                    <p>关于我们</p>
+                    <p>公司招聘</p>
+                    <p>公司团队</p>
+                    <p>版权</p>
+                    <p>使用条款</p>
+                    <p>隐私政策</p>
+                    <p>联系我们</p>
+                </div>
+                <div class="col-lg-3 col-md-6 col-xs-6 text-center weibu4">
+                      <h3>公司</h3>
+                    <p>关于我们</p>
+                    <p>公司招聘</p>
+                    <p>公司团队</p>
+                    <p>版权</p>
+                    <p>使用条款</p>
+                    <p>隐私政策</p>
+                    <p>联系我们</p></div> 
+               
+               
+            </div>
+        </div>
+         <div class="container-fluid hezuo2">
+             <div class="row">
+           <div class="col-lg-6" >@2017 comlogo all  right reserved</div>
+           <div class="col-lg-6 text-right">home   about us   contact us</div>
+         </div>
+         </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'boddy'
+}
+</script>
+
+<style>
+.hezuo2{
+ color: white;
+    background-color: #2E2E2E;
+    height: 45px;
+    line-height: 45px;
+}
+.logos1{
+    
+    height: 190px;
+    background:url('../assets/logos.png')no-repeat center 0;
+}
+.logos2{
+    height: 190px;
+    background:url('../assets/logos.png')no-repeat center -180px;
+}
+.logos3{
+    height: 190px;
+    background:url('../assets/logos.png')no-repeat center -360px;
+}
+.logos4{
+    height: 190px;
+    background:url('../assets/logos.png')no-repeat center -540px;
+}
+.logos5{
+    height: 190px;
+    background:url('../assets/logos.png')no-repeat center -720px;;
+}
+.hezuo{
+    color: white;
+    margin-top: 80px;
+    background-color:#2C8FBA;
+}
+.tupian{
+    padding-top: 50px;
+}
+.tupian1{
+    height: 226px;
+    background: url('../assets/pic01.jpg')no-repeat center/100%;
+    overflow: hidden;
+}
+.tupian2{
+    height: 226px;
+    background: url('../assets/pic02.jpg')no-repeat center/100%;
+}
+.tupian3{
+    height: 226px;
+    background: url('../assets/pic03.jpg')no-repeat center/100%;
+}
+.tupian4{
+    height: 226px;
+    background: url('../assets/pic04.jpg')no-repeat center/100%;
+}
+.tupian5{
+    height: 226px;
+    background: url('../assets/pic05.jpg')no-repeat center/100%;
+}
+.tupian6{
+    height: 226px;
+    background: url('../assets/pic06.jpg')no-repeat center/100%;
+}
+.tupian7{
+    height: 226px;
+    background: url('../assets/pic07.jpg')no-repeat center/100%;
+}
+.tupian8{
+    height: 226px;
+    background: url('../assets/pic08.jpg')no-repeat center/100%;
+}
+.banner{
+    background:url('../assets/bg.jpg')  center no-repeat/ 100% 100%;
+     
+}
+.col-lg-offset-1 {
+    padding-top: 100px;
+}
+.gray1{
+    background-color: #f2f2f2;
+}
+.list-con{
+    padding-top: 100px;
+}
+.list01{
+    height: 112px;
+    padding-left: 138px;
+    background: url('../assets/icons.png') left top no-repeat;
+    margin-bottom: 39px;
+}
+.list02{
+    height: 112px;
+    padding-left: 138px;
+    background: url('../assets/icons.png') left -112px no-repeat;
+    margin-bottom: 39px;
+}
+.list03{
+    height: 112px;
+    padding-left: 138px;
+    background: url('../assets/icons.png') left -224px no-repeat;
+    margin-bottom: 39px;
+}
+.list04{
+    height: 112px;
+    padding-left: 138px;
+    background: url('../assets/icons.png') left -336px no-repeat;
+    margin-bottom: 39px;
+}
+.list05{
+    height: 112px;
+    padding-left: 138px;
+    background: url('../assets/icons.png') left -448px no-repeat;
+    margin-bottom: 39px;
+}
+.list06{
+    height: 112px;
+    padding-left: 138px;
+    background: url('../assets/icons.png') left -560px no-repeat;
+    margin-bottom: 39px;
+}
+</style>
---
+++ src/components/header1.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="hello">
+  
+<nav class="navbar navbar-inverse">
+  <div class="container">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#"><img src="../assets/logo.png" alt=""></a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
+        <li><a href="#">Link</a></li>
+         <li><a href="#">Link</a></li>
+          <li><a href="#">Link</a></li>
+      </ul>
+      <form class="navbar-form navbar-right">
+        <div class="form-group">
+          <input type="text" class="form-control" placeholder="搜索">
+        </div>
+        <button type="submit" class="btn btn-default">Submit</button>
+      </form>
+    
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+ 
+  </div>
+
+</template>
+
+<script>
+export default {
+ 
+  data () {
+    return {
+     
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.navbar-brand{
+  padding: 10px 15px;
+}
+.navbar {
+    margin-bottom:0px;
+   
+}
+</style>
